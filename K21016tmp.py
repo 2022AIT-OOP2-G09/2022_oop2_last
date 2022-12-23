574b0513bd7440426de5cb8cb33e3078e25c5b64
iu-A <imaizumi2002@gmail.com>
2022-12-23 09:36:50 +0900

    新規登録の関数の中身を記述(一部)

--- K21016tmp.py
+++ K21016tmp.py
@@ -1,9 +1,21 @@
 from flask import Flask, request, render_template
 import sqlite3
 
+
+#データベースにIDとパスワードを受付けて保存する処理
 app = Flask(__name__)
 
 @app.route('/touroku', methods = ["POST","POST"])
 def touroku():
-    
-    pass
+    username = request.form.get("id")
+    password = request.form.get("password")
+    password2 = request.form.get("password2")
+    if password == password2:
+        dbname = 'ID_pass_database.db'
+        conn = sqlite3.connect(dbname)
+        cur = conn.cursor()
+        cur.execute(f'INSERT INTO user_ID_Pass(name,pass) values({username},{password}),')
+        conn.commit()
+        cur.close()
+        conn.close()
+
