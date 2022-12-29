cd67e71030edb16b61d05944ff616a6f6a84105c
esukarugon <dededeoroshi0108@gmail.com>
2022-12-29 23:23:54 +0900

    新規登録機能の追加

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from flask import Flask
+from flask import jsonify
 from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user
 
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+
+import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 from datetime import datetime
+import time
 import pytz
+
 
 # create the extension
 db = SQLAlchemy()
@@ -43,6 +51,10 @@ class User_ID_Pass(UserMixin, db.Model):
 def load_user(user_id):
     return User_ID_Pass.query.get(int(user_id))
 
+@app.route('/')
+def index():
+    return render_template('login.html')
+
 # ログインページ
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -63,7 +75,63 @@ def login():
     else:   # request.method == 'GET'
         return render_template('login.html')
 
+@app.route('/signUp_form', methods = ["GET","POST"])
+def touroku():
+    if request.method == 'GET':
+        print('ページ移動しました')
+        return render_template('signUp_form.html')
+    elif request.method == 'POST':
+        print('入力を受け付けました')
+        username = request.form.get("id")
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
+        print(username)
+        print(password)
+        print(password2)
+        if password == password2:
+            dbname = 'ID_pass_database.db'
+            conn = sqlite3.connect(dbname)
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM User_ID_Pass')
+            count = 0
+            for row in cur:
+                if row[1] == username:
+                    count=count+1
+            if count > 0:
+                print('この名前は使われています') 
+                ##ret = {
+                ##        "message": "既に使用されている名前です",##メッセージが出てないパスワードの不一致は確認
+                ##}
+                cur.close()
+                conn.close()
+                return render_template('signUp_form.html')
+            else:
+                params = (username,password)
+                cur.execute('INSERT INTO User_ID_Pass values(NULL,?,?)',params)
+                print('登録が完了しました')
+                conn.commit()
+                cur.close()
+                conn.close()
+                ##ret = {
+                ##        "message": "登録が完了しました",##メッセージが出てないパスワードの不一致は確認
+                ##    }
+                return render_template('login.html')
+        else:
+            print('パスワードが一致していません')
+            ##ret = {
+            ##        "message": "パスワードが一致しません",##メッセージが出てないパスワードの不一致は確認
+            ##    }    
+            return render_template('login.html') 
+    
+
+
+
 # TOPページ担当の人へ
 @app.route('/index')
 def toppage():
     return render_template('index.html')
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
