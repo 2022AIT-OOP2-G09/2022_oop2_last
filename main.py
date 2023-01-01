4adf03301edd5b47a5982bd717f0a388a1d55f8f
esukarugon <dededeoroshi0108@gmail.com>
2023-01-01 15:20:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask import jsonify
 from flask import render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user
+
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
@@ -10,46 +9,10 @@ from watchdog.observers import Observer
 
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
-import os
-
-from datetime import datetime
-import time
-import pytz
 
 
-# create the extension
-db = SQLAlchemy()
 # create the app
 app = Flask(__name__)
-# configure the SQLite database, relative to the app instance folder
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ID_pass_database.db'
-# configure the secret key
-app.config['SECRET_KEY'] = os.urandom(24)
-# initialize the app with the extendion
-db.init_app(app)
-
-# ログイン機能に必要な設定
-login_manager = LoginManager()   # LoginManagerクラスのインスタンスを作成
-# ログイン機能を持ったLoginManagerと今回のアプリを紐づける
-login_manager.init_app(app)  
-
-# 投稿DBの作成
-class Tweet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50), nullable=False)
-    content = db.Column(db.String(300), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
-
-# ユーザーDBの作成
-class User_ID_Pass(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)   # ユーザー名は重複なし
-    password = db.Column(db.String(12), nullable=False)   # パスワードは12文字以内
-    
-# ログインに必要
-@login_manager.user_loader
-def load_user(user_id):
-    return User_ID_Pass.query.get(int(user_id))
 
 @app.route('/')
 def index():
@@ -85,14 +48,14 @@ def login():
                 print('パスワードが間違っています')
                 cur.close()
                 conn.close()
-                ount_name=0
+                count_name=0
                 count_pass=0
                 return render_template('login.html')
             else:
                 print('ログイン完了しました')
                 cur.close()
                 conn.close()
-                ount_name=0
+                count_name=0
                 count_pass=0
                 return render_template('home.html')
 
@@ -149,8 +112,6 @@ def touroku():
             return render_template('login.html') 
     
 
-
-# TOPページ担当の人へ
 @app.route('/index')
 def toppage():
     return render_template('index.html')
